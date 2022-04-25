61b3b741f8c7094b7abedba534f9e75da28f3c4e
OPSolutions <opsolutions@OPSolutionss-MacBook-Pro.local>
2022-04-25 09:48:29 +0800

    with other mini projects

---
+++ random-picker-js/main.js
@@ -0,0 +1,23 @@
+const tagsEl = document.getElementById('tags');
+const textareaEl = document.getElementById('#textarea');
+
+textareaEl.focus();
+
+textareaEl.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+  console.log(tags);
+
+  tagsEl.innerHTML = ''
+
+  tags.forEach(tag => {
+      const tagEl = document.createElement('span')
+      tagEl.classList.add('tag')
+      tagEl.innerText = tag
+      tagsEl.appendChild(tagEl)
+      // console.log(tagEl);
+  })
+}
